b91d963c8ad7ec681a32c1d5aabb779b2a4c3ba6
Travis Stebbins <stebbins.travis@gmail.com>
2017-10-13 21:32:50 -0500

    Implement basic game classes
    GameManager and Player

---
+++ IGG/Assets/Scripts/GameManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	// INSTANCE
+	static GameManager m_instance;
+	public static GameManager instance
+	{
+		get
+		{
+			if (m_instance == null)
+			{
+				m_instance = FindObjectOfType<GameManager>();
+			}
+			return m_instance;
+		}
+	}
+
+	// SERIALIZE FIELD VARIABLES
+	[SerializeField] Player player1;
+	[SerializeField] Player player2;
+
+	// CLASS VARIABLES
+	int m_predatorIndex;
+	public readonly float predatorSpeed = 5;
+	public readonly float preySpeed = 5;
+
+	// PROPERTIES
+	public int predatorIndex
+	{
+		get
+		{
+			return m_predatorIndex;
+		}
+		private set
+		{
+			m_predatorIndex = value;
+			switch(m_predatorIndex)
+			{
+				case 0:
+					player1.isPredator = true;
+					player2.isPredator = false;
+					break;
+				case 1:
+					player1.isPredator = false;
+					player2.isPredator = true;
+					break;
+			}
+		}
+	}
+		
+	// UNITY FUNCTIONS
+	void Awake()
+	{
+		if (instance != this)
+		{
+			Destroy (this.gameObject);
+		}
+	}
+
+	void Start()
+	{
+		player1.playerIndex = 0;
+		player2.playerIndex = 1;
+		predatorIndex = 0;
+	}
+}
---
+++ IGG/Assets/Scripts/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// CLASS VARIABLES
+	bool m_isPredator;
+	float speed;
+
+	// PROPERTIES
+	public int playerIndex { get; set; }
+	public bool isPredator
+	{
+		get
+		{
+			return m_isPredator;
+		}
+		set
+		{
+			m_isPredator = value;
+			speed = m_isPredator ? GameManager.instance.predatorSpeed : GameManager.instance.preySpeed;
+		}
+	}
+
+	// UNITY FUNCTIONS
+	void Update ()
+	{
+		if (playerIndex == 0)
+		{
+			transform.position += Vector3.right * Input.GetAxis ("Player1Horizontal") * speed * Time.deltaTime;
+			transform.position += Vector3.up * Input.GetAxis ("Player1Vertical") * speed * Time.deltaTime;
+		}
+		else if (playerIndex == 1)
+		{
+			transform.position += Vector3.right * Input.GetAxis ("Player2Horizontal") * speed * Time.deltaTime;
+			transform.position += Vector3.up * Input.GetAxis ("Player2Vertical") * speed * Time.deltaTime;
+		}
+	}
+}

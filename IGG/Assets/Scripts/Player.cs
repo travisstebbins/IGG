13a8192989476107900b53562c8d7bb656467f83
Travis Stebbins <stebbins.travis@gmail.com>
2017-10-13 21:48:19 -0500

    Add collission detection to players

--- IGG/Assets/Scripts/Player.cs
+++ IGG/Assets/Scripts/Player.cs
@@ -24,7 +24,7 @@ public class Player : MonoBehaviour
 	}
 
 	// UNITY FUNCTIONS
-	void Update ()
+	void Update()
 	{
 		if (playerIndex == 0)
 		{
@@ -35,6 +35,14 @@ public class Player : MonoBehaviour
 		{
 			transform.position += Vector3.right * Input.GetAxis ("Player2Horizontal") * speed * Time.deltaTime;
 			transform.position += Vector3.up * Input.GetAxis ("Player2Vertical") * speed * Time.deltaTime;
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{
+		if (coll.gameObject.GetComponent<Player>() != null)
+		{
+			Debug.Log ("ran into player " + coll.gameObject.GetComponent<Player> ().playerIndex);
 		}
 	}
 }
